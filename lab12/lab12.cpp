ae9c389c8b1b10da90aa93966995ceeda1c6d90b
Sydney Holderbein <sholderbein001@student.butte.edu>
2017-01-31 22:30:49 +0000

    Lab1.2 Commit

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,12 +1,38 @@
-    |--------------|
-    |   ~  ----    |
-    |              |
-    |   @   #   %  |
-    |   &   $   *  |
-    |   ç   ¢      | 
-    |       ¶   §  | 
-    |   •   ∞   £  |
-    |   ¡   ™   §  |
-    |              |
-    |      ( )     |
-    |--------------|
+//Created By: Sydney Holderbein
+// Date Created: January 31, 2017
+
+#include <iostream>
+using namespace std;
+int main ()
+
+
+{
+    cout<< "|----------------------|"; 
+    cout<< endl;
+    cout<< "|     ~    -----       |";
+    cout<< endl;
+    cout<< "|                      |";
+    cout<< endl;
+    cout<< "|   *    $    %   &    |";
+    cout<< endl;
+    cout<< "|   ^    #    !   @    |";
+    cout<< endl;
+    cout<< "|   ^    ☻    ♥   ♦    |";
+    cout<< endl;
+    cout<< "|   E    ◘    N   ß    |";
+    cout<< endl;
+    cout<< "|   H    á    ╖    ╪   |";
+    cout<< endl;
+    cout<< "|   c    J    í    ▌   |";
+    cout<< endl;
+    cout<< "|                      |";
+    cout<< endl;
+    cout<< "|         (  )         |";
+    cout<< endl;
+    cout<< "|----------------------|";
+    cout<< endl;
+   
+    return 0;
+}
+ 
+ 
