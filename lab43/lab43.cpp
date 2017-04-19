1336f7c4cf57e6727d0f1043bc4d7acecc431858
Sydney Holderbein <sholderbein001@student.butte.edu>
2017-04-19 05:40:08 +0000

    Lab 4.3 Submission

---
+++ lab43/lab43.cpp
@@ -0,0 +1,44 @@
+// Created By; Sydney Holderbein
+// Created On: April 18, 2017
+
+//This program will simulate an online shopping cart. 
+
+#include <iostream>
+using namespace std;
+
+class ShoppingCart{
+    private:
+        const int SIZE=3;
+        
+    public:
+        int cartArray[int cartSize]{0,0,0};
+        int tvSize = 0;
+        int tvCost = 0;
+        int numtvLeft = 0;
+        int getCartArraySize();
+        void setCartArraySize(int SIZE);
+        ShoppingCart;
+        ShoppingCart[]{0,0,0};
+        
+    }
+void setCartArraySize(10){
+        cartSize = SIZE;
+}
+int getCartArray{
+        return cartSize;
+}
+ShoppingCart::ShoppingCart{
+    
+}
+
+
+
+int main(){
+    
+    cout << "Welcome to your shop";
+    for (int i=0; i<2; i++){
+    cout << cartArray[i];
+    
+    
+}
+}
